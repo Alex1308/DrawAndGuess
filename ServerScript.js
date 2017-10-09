4c230e12a1da4f6d90fbb935c7a3876009aac481
Alex1308 <alexander.gehling@gmail.com>
2017-10-09 17:14:19 +0200

    Implemented server using Express.

--- ServerScript.js
+++ ServerScript.js
@@ -3,8 +3,13 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.use(express.static(__dirname + '/node_modules'));
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "Style.css");
+});
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/Client.html');
 });
 
-server.listen(4200);  
+server.listen(4200);
