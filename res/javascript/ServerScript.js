c2b269e7f5e43af5adf340c931a5dbdf8591926c
Mathias <mathias-1992@live.dk>
2017-10-31 13:44:54 +0100

    Updated folders - added res folder

---
+++ res/javascript/ServerScript.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "Style.css");
+});
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/Index.html');
+});
+
+server.listen(4200);
