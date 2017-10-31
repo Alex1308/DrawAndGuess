137d45fb605a88f4145237e8b36ecc515d1b9b0b
Alex1308 <alexander.gehling@gmail.com>
2017-10-31 17:18:59 +0100

    Fixed server communication to client using Express.

---
+++ ServerScript.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = 4200;
+
+var path = require('path');
+
+app.use(express.static('res'));
+
+server.listen(port);
+console.log("Listening on port " + port);
+
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
