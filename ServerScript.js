31c0d1b3e1a75148ba5e11d7eb9e68ef53e60243
Alex1308 <alexander.gehling@gmail.com>
2017-11-02 15:55:17 +0100

    Added username and random word chooser. In the process of creating list of client IDs

--- ServerScript.js
+++ ServerScript.js
@@ -10,14 +10,70 @@ var options = {
     index: "html/index.html"
 };
 
+var clients = [];
+
 app.use('/', express.static('res', options));
+
 
 server.listen(port);
 console.log("Listening on port " + port);
 
-
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
+    console.log('A user connected');
+    clients.add(socket.id);
+    console.log(clients[0]);
+    socket.send('request nickname');
+    socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     });
+    socket.on('disconnect' , function() {
+        clients.remove(socket.id);
+        console.log(clients[0]);
+    })
 });
+
+
+var chosenWord;
+
+var possibleWords = [
+    "Avocado",
+    "Airplane",
+    "Bee",
+    "Busta rhymes",
+    "Alberto Contador",
+    "Reddit",
+    "Monitor",
+    "Captain Morgan",
+    "Zeppelin",
+    "Burger",
+    "Cannoo",
+    "Supermarket",
+    "Torpedo",
+    "Offside",
+    "Motorbike",
+    "Rodeo",
+    "Hangover",
+    "Kangaroo",
+    "Tank",
+    "Panther",
+    "Gorilla",
+    "Attack Helicopter",
+    "Blue Whale",
+    "Christmas Eve",
+    "Santa Claus",
+    "Titanic",
+    "Donald Trump",
+    "Restaurant",
+    "Javascript",
+    "Wok",
+    "Liverpool FC",
+    "Internet",
+    "Calculator",
+    "Guitar"
+];
+
+
+function ChooseRandomWord() {
+    var random = Math.floor((Math.random()* possibleWords.length));
+    return possibleWords[random];
+}
